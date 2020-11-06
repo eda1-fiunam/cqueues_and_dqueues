bc341fa0aaef4fc194f4cc1158f7cc8d465c240e
eda1-fiunam <eda1.fiunam@yahoo.com.mx>
2020-11-05 20:13:34 -0600

    Hice cambios estéticos

--- CQueue.c
+++ CQueue.c
@@ -32,25 +32,25 @@
 
 typedef struct
 {
-	int* queue;
+	int    *queue;
 	size_t front;
 	size_t back;
 	size_t len;
 	size_t capacity;
 } CQueue;
 
-CQueue* CQueue_New( size_t n );
-void CQueue_Delete( CQueue** this );
-void CQueue_Enqueue( CQueue* this, int val );
-int CQueue_Dequeue( CQueue* this );
-int CQueue_Peek( CQueue* this );
-bool CQueue_IsFull( CQueue* this );
-bool CQueue_IsEmpty( CQueue* this );
-void CQueue_MakeEmpty( CQueue* this );
-size_t CQueue_Len( CQueue* this );
-size_t CQueue_Capacity( CQueue* this );
+CQueue* CQueue_New(       size_t capacity );
+void    CQueue_Delete(    CQueue** this );
+void    CQueue_Enqueue(   CQueue* this, int val );
+int     CQueue_Dequeue(   CQueue* this );
+int     CQueue_Peek(      CQueue* this );
+bool    CQueue_IsFull(    CQueue* this );
+bool    CQueue_IsEmpty(   CQueue* this );
+void    CQueue_MakeEmpty( CQueue* this );
+size_t  CQueue_Len(       CQueue* this );
+size_t  CQueue_Capacity(  CQueue* this );
 
-CQueue* CQueue_New( size_t _capacity )
+CQueue* CQueue_New( size_t capacity )
 {
    return NULL;
 }
